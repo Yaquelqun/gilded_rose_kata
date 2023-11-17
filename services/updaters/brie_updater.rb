a864d8b3573db85fe030cf7f52c413dff1692b2b
Yaquelqun <sandjiv.parassou@gmail.com>
2023-11-17 15:02:56 +0100

    implement brie updater

---
+++ services/updaters/brie_updater.rb
@@ -0,0 +1,28 @@
+module Updaters
+  class BrieUpdater
+    attr_reader :item
+
+    def initialize(item)
+      @item = item
+    end
+
+    def tick
+      update_sell_in
+      update_quality
+    end
+
+    def update_sell_in
+      item.sell_in -= 1
+    end
+
+    def update_quality
+      item.quality = [item.quality + (expired? ? 2 : 1), 50].min
+    end
+
+    private
+
+    def expired?
+      item.sell_in.negative?
+    end
+  end
+end
