e480259fd8029057fd70e0ceb298f8c5de2fd299
Yaquelqun <sandjiv.parassou@gmail.com>
2023-11-17 15:04:30 +0100

    implement sulfuras updater

--- services/updater_manager.rb
+++ services/updater_manager.rb
@@ -3,7 +3,7 @@ Dir["./services/updaters/*.rb"].each { |file| require file }
 module UpdaterManager
   def self.find_updater_for(item)
     return Updaters::BrieUpdater if item.name == "Aged Brie"
-    # return Updaters::SulfurasUpdater if item.name.downcase.include?("sulfuras")
+    return Updaters::SulfurasUpdater if item.name.downcase.include?("sulfuras")
     # return Updaters::BackstagePassUpdater if item.name.downcase.include?("backstage passes")
     # return Updaters::ConjuredItemUpdater if item.name.downcase.include?("conjured")
 
---
+++ services/updaters/sulfuras_updater.rb
@@ -0,0 +1,11 @@
+module Updaters
+  class SulfurasUpdater
+    attr_reader :item
+
+    def initialize(item)
+      @item = item
+    end
+
+    def tick;end
+  end
+end
